6048f816c37569f002200ee1f6a76fbb1c4b5ba1
terence <terenceshawn18@outlook.com>
2023-12-12 13:40:42 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,32 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self,name="fido",breed="Corgi"):
+        self.name=name
+        self.breed=breed
+    # breed
+    def get_breed(self):
+        return self._breed
+    def set_breed(self,breed):
+        if breed in APPROVED_BREEDS:
+            
+            self._breed=breed
+        else:
+            print("Breed must be in list of approved breeds.")
+    # name   
+    def get_name(self):
+        print("getting name")
+        return self._name
+    
+    def set_name(self,name):
+        if not isinstance(name,str):
+            print("Name must be string between 1 and 25 characters.")
+        elif 1<= len(name) <= 25:
+            self._name=name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    name=property(get_name,set_name)
+    breed=property(get_breed,set_breed)
+    
+tp=Dog("","Pointer")
+
